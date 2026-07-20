ab10c858528d4b42e36ee9302047bd13ed4197ea
Robert Simionescu <simionescurobert23@gmail.com>
2026-07-20 13:26:51 +0300

    rework camera systemte

--- Assets/Camera/followPlayer.cs
+++ Assets/Camera/followPlayer.cs
@@ -6,32 +6,27 @@ using UnityEngine.Tilemaps;
 
 public class followPlayer : MonoBehaviour
 {
-
-    public GameObject  player;
-    public GameObject  room;
-    public Bounds      levelBounds;
-    public float       cameraSpeed = 1.5f;
-    public float       cameraOffsetX = 4f;
-    public float       cameraOffsetY = 2f;
-    public float       currOffset   = 0f;
-    private Camera     cameraObject;
+    //Needed References to gameObject properties
+    private Camera                  cameraObject;
     [SerializeField]private Vector2 cameraSize;
-    [SerializeField]private Bounds cameraBounds;
-    public Tilemap groundTilemap;
+    [SerializeField]private Bounds  cameraBounds;
 
-    public bool isMoving = false;
+    //External References used
+    public Tilemap      groundTilemap;
+    public GameObject   player;
+    public Vector3      playerPos;
+    public GameObject   room;
+    public Bounds       levelBounds;
+
+    //Camera Speed
+    public float       cameraSpeed;
+    public float       defaultCameraSpeed = 1.5f;
+
+    //Dead Zone parameters
+    public bool    isMoving  = false;
     public Vector3 targetPos;
-    public float distanceX;
-    public float distanceY;
-    public Vector3 playerPos;
-
-    //TODO: Implement system to keep camera centered on mean of position ion last x milseconds
-
-    public float medianTimer = 0.3f;
-    public float currMedianTimer;
-    public Vector3 medianPosition;
-    public int positionsUsed;
-    private Vector3 tempMedian;
+    public float   distanceX;
+    public float   distanceY;
 
     void Awake()
     {
@@ -41,17 +36,16 @@ public class followPlayer : MonoBehaviour
             x = cameraObject.orthographicSize * cameraObject.aspect,
             y = cameraObject.orthographicSize
         };
-        cameraOffsetX = cameraSize.x;
-
-        currMedianTimer = medianTimer;
-        medianPosition  = playerPos;
-        positionsUsed   = 0;
+        cameraSpeed   = defaultCameraSpeed;
     }
 
     public void CalculateCameraBounds()
     {
         //Start camera at 0
         cameraObject.transform.position = new Vector3(0, 0, cameraObject.transform.position.z);
+
+        //Minimums of camera position based on bounds and camera size
+        //As in, place camera left corner within level
         float newXMin = levelBounds.min.x + cameraSize.x;
         float newYMin = levelBounds.min.y + cameraSize.y;
         float newXMax = levelBounds.max.x - cameraSize.x;
@@ -64,52 +58,9 @@ public class followPlayer : MonoBehaviour
         cameraBounds.SetMinMax(cameraMinBound, cameraMaxBound);
     }
 
-    public void SetMedianPosition()
-    {
-        if(currMedianTimer > 0){
-            currMedianTimer -= Time.deltaTime;
-
-            tempMedian.x += playerPos.x;
-            tempMedian.y += playerPos.y;
-            tempMedian.z  = playerPos.z;
-
-            positionsUsed += 1;
-        }
-        else
-        {
-            medianPosition.x = (tempMedian.x / positionsUsed) + currOffset;
-            medianPosition.y = tempMedian.y / positionsUsed;
-            medianPosition.z = tempMedian.z;
-
-            //Resets
-            currMedianTimer = medianTimer;
-            tempMedian      = Vector3.zero;
-            positionsUsed   = 0;
-        }
-
-    }
-
+    //Make sure camera does not show parts that are not in the level, outer bounds
     private Vector3 GetCameraPosition(Vector3 currentPos)
     {
-        bool isWallsliding = player.GetComponent<PlayerCharacter>().isWallSliding;
-        bool isWallJumping = player.GetComponent<PlayerCharacter>().isWallJumping;
-
-        Vector3 startPos = currentPos;
-        if(isMoving){
-            // else
-            //     currentPos.x = startPos.x;
-
-            // if(player.GetComponent<Rigidbody2D>().linearVelocityY > 1f)
-            //     currentPos.y += cameraOffsetY;
-            // else if(player.GetComponent<Rigidbody2D>().linearVelocityY < 1f && isWallsliding && !isWallsliding)
-            //     currentPos.y -= cameraOffsetY;
-            // else if(player.GetComponent<Rigidbody2D>().linearVelocityY == 0)
-            //     currentPos.y = player.transform.position.y;
-
-            // if(isWallJumping || isWallsliding)
-            //     currentPos.y += cameraOffsetY;
-        }
-
         Vector3 newPos = new()
         {
             //Default to center of bounds if camera is larger than room
@@ -124,18 +75,18 @@ public class followPlayer : MonoBehaviour
             z = transform.position.z //Z is constant
         };
 
-
         return newPos;
     }
 
-    public void SetIsMoving(Vector3 currentPos)
+
+    // Dead zone enforcement
+    public void SetIsMoving(float distanceX, float distanceY)
     {
-        //TODO: Make values 2f etc variables for ease of config
         if (isMoving)
         {
             if(
-                Math.Abs(targetPos.x - currentPos.x) < 0.1f &&
-                Math.Abs(targetPos.y - currentPos.y) < 0.1f
+                distanceX < 0.2f &&
+                distanceY < 0.2f
             ){
                 isMoving = false;
             }
@@ -143,37 +94,44 @@ public class followPlayer : MonoBehaviour
         else
         {
             if(
-                Math.Abs(targetPos.x - currentPos.x) > 2.5f ||
-                Math.Abs(targetPos.y - currentPos.y) > 1f
+                distanceX > 4f ||
+                distanceY > 2f
             ){
+                Debug.Log("IsMoving");
                 isMoving = true;
-                if(player.GetComponent<Rigidbody2D>().linearVelocityX > 0)
-                    currentPos.x += cameraOffsetX;
-                else if(player.GetComponent<Rigidbody2D>().linearVelocityX < 0)
-                    currentPos.x -= cameraOffsetX;
             }
         }
     }
 
-    //TODO: Implement a system that only allows camera movement if the distance between last movement and current position is bigger tha a set value
-
-
-    // Update is called once per frame
+    //If the player is moving too fast for the camera, update it's speed
+    void UpdateCameraSpeedToPlayerSpeed()
+    {
+        float playerSpeed = Math.Abs(player.GetComponent<Rigidbody2D>().linearVelocityY);
+        if(playerSpeed > 8f)
+        {
+            if(cameraSpeed < playerSpeed)
+                cameraSpeed += defaultCameraSpeed * 10 * Time.deltaTime; //TODO: 10 is arbitrary and can be changed for a variable if needed
+            else
+                cameraSpeed = playerSpeed;
+        }
+        else
+        {
+            cameraSpeed = defaultCameraSpeed;
+        }
+    }
     void LateUpdate()
     {
         playerPos = player.transform.position;
+        targetPos = GetCameraPosition(playerPos);
 
-        SetMedianPosition();
-        if(Math.Abs(player.GetComponent<Rigidbody2D>().linearVelocityY) > 2)
-            targetPos = GetCameraPosition(new Vector3(medianPosition.x, playerPos.y, playerPos.z));
-        else
-            targetPos = GetCameraPosition(medianPosition);
-
-        SetIsMoving(targetPos);
-
+        //Distances from player to camera
         distanceX = Math.Abs(targetPos.x - transform.position.x);
         distanceY = Math.Abs(targetPos.y - transform.position.y);
+
+        SetIsMoving(distanceX, distanceY);
+
         if(isMoving)
+            UpdateCameraSpeedToPlayerSpeed();
             transform.position = Vector3.Lerp(transform.position,
                                               targetPos,
                                               cameraSpeed * Time.deltaTime);
